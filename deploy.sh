4134d2983d0d10ff74715e77cb167653159ff54d
Qihang Zhang <qihangz.work@gmail.com>
2024-02-15 09:05:39 -0800

    2024-02-15 update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 
 python ./python_scripts/merge_mkdocs.py
 
-python ./python_scripts/generate_md.py \
+python3 ./python_scripts/generate_md.py \
 --getup_threshold 7.75 \
 --recent_days 30 \
 --name Qihang

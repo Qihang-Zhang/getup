caf834a81d19a85a46e9fa9e0fcf83fb6e057916
Qihang Zhang <qihangz.work@gmail.com>
2024-02-15 09:02:35 -0800

    2024-02-15 update

--- getup.sh
+++ getup.sh
@@ -1,11 +1,10 @@
-python generate_csv.py --cutoff 5
+python3 generate_csv.py --cutoff 5
 
 git add .
 git commit -m "$(date +%Y-%m-%d) update"
 git push
 
 # below is to print some interesting message, feel free to delete or modify it
-clear
 echo "==========================================================================="
 echo "Morning! Have a nice day!"
 echo "==========================================================================="

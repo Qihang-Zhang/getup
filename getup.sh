bf7afe09107121aa3e471a002cd446aa387f0ca8
Qihang Zhang <qihangz.work@gmail.com>
2024-03-01 13:27:21 -0800

    2024-03-01 update

--- getup.sh
+++ getup.sh
@@ -6,8 +6,8 @@ git commit -m "$(date +%Y-%m-%d) update"
 git push
 
 # below is to print some interesting message, feel free to delete or modify it
-echo "==========================================================================="
-echo "Morning! Have a nice day!"
+# echo "==========================================================================="
+# echo "Morning! Have a nice day!"
 echo "==========================================================================="
 figlet Morning!
 figlet Have a nice day!

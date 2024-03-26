700c89c78779db28c9fb10ffd4cddea53b2b28e8
Qihang Zhang <qihangz.work@gmail.com>
2024-03-26 16:26:09 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ python3 ./python_scripts/merge_mkdocs.py
 python3 ./python_scripts/generate_md.py \
 --getup_threshold 6.75 \
 --recent_days 30 \
+--tz "America/Vancouver" \
 --name Qihang
                       
 mkdocs gh-deploy --force

7d17d009476b89357c1d614bcaa921334dba8c90
Qihang Zhang <qihangz.work@gmail.com>
2024-03-26 15:46:30 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,3 @@
-git clone https://github.com/Qihang-Zhang/getup-core.git
-
-ln -s getup-core/* .
-
-pip install -r requirements.txt
-
-git pull
-python3 generate_csv.py --cutoff 5
-
-git add .
-git commit -m "$(date +%Y-%m-%d) update"
-git push
-
 python3 ./python_scripts/merge_mkdocs.py
 
 python3 ./python_scripts/generate_md.py \
---
+++ env.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Qihang-Zhang/getup-core.git
+
+ln -s getup-core/* .
+
+pip install -r requirements.txt
